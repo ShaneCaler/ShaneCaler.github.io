119b812651168476a10863655b309c5cac66a42c
Shane <scaler0222@gmail.com>
2019-04-22 12:14:56 -0700

    small fixes

---
+++ css/precache-manifest.3cc7ed726e1111ff886a9b5f3a66ef49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0990d76a4ae7b0221b9f",
+    "url": "/static/js/main.0fae088c.chunk.js"
+  },
+  {
+    "revision": "90678800688619097b7e",
+    "url": "/static/js/2.210509be.chunk.js"
+  },
+  {
+    "revision": "0990d76a4ae7b0221b9f",
+    "url": "/static/css/main.f30edb80.chunk.css"
+  },
+  {
+    "revision": "132fc1cb1789634f9dc886c6077b5c05",
+    "url": "/index.html"
+  }
+];
