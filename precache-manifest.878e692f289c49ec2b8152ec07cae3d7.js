f725b5fbc5f3707597bde22bd1d8a33d23b6d4ae
Shane <scaler0222@gmail.com>
2019-04-21 21:48:38 -0700

    first commit

---
+++ precache-manifest.878e692f289c49ec2b8152ec07cae3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "844091efaf0905979496",
+    "url": "/static/js/main.97c8ecb9.chunk.js"
+  },
+  {
+    "revision": "90678800688619097b7e",
+    "url": "/static/js/2.210509be.chunk.js"
+  },
+  {
+    "revision": "844091efaf0905979496",
+    "url": "/static/css/main.f30edb80.chunk.css"
+  },
+  {
+    "revision": "1b29c05a57a592137d6a2b07d326e946",
+    "url": "/index.html"
+  }
+];
